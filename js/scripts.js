4b2c7cc947e9252937278ca564b0aac817ba0bd1
hannah melendy and marina poltorak <marina@>
2019-06-04 16:35:33 -0700

    add files and code for flip card

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".JavaScript").click(function() {
+    $(".JavaScript").children().toggle();
+  });
+
+  $(".operators").click(function() {
+    $(".operators").children().toggle();
+  });
+});
